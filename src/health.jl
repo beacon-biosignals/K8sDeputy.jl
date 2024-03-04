b3cf654bcae5e0261c2e9acdb2283f15ec43a459
Curtis Vogt <curtis.vogt@gmail.com>
2024-03-04 16:01:29 -0600

    Add docstrings to health check functions

--- src/health.jl
+++ src/health.jl
@@ -5,15 +5,47 @@ mutable struct Deputy
     shutdown_handler_timeout::Second
 end
 
+"""
+    Deputy(; shutdown_handler=nothing, shutdown_handler_timeout::Period=Second(5))
+
+Construct an application `Deputy` which provides health check endpoints.
+
+## Keywords
+
+- `shutdown_handler` (optional): A zero-argument function which allows the user to provide
+  a custom callback function for when `shutdown(::Deputy)` is called.
+- `shutdown_handler_timeout::Period` (optional): Specifies the maximum execution duration of
+  a `shutdown_handler`.
+"""
 function Deputy(; shutdown_handler=nothing, shutdown_handler_timeout::Period=Second(5))
     return Deputy(false, false, shutdown_handler, shutdown_handler_timeout)
 end
 
+"""
+    readied(deputy::Deputy) -> Nothing
+
+Mark the application as "ready". Sets the readiness endpoint to respond with successful
+responses.
+"""
 function readied(deputy::Deputy)
     deputy.ready = true
     return nothing
 end
 
+"""
+    shutdown(deputy::Deputy) -> Nothing
+
+Initiates a shutdown of the application by:
+
+1. Setting the liveness endpoint to respond with failures.
+2. Executing the deputy's `shutdown_handler` (if defined).
+3. Exiting the current Julia process.
+
+If a `deputy.shutdown_handler` is defined it must complete within the
+`deputy.shutdown_handler_timeout` or a warning will be logged and the Julia process will
+immediately exit. Any exceptions that occur in the `deputy.shutdown_handler` will also be
+logged and result in the Julia process exiting.
+"""
 function shutdown(deputy::Deputy)
     # Abend if already shutting down
     deputy.shutting_down && return nothing
--- src/server.jl
+++ src/server.jl
@@ -5,6 +5,18 @@ function _default_port()
     return haskey(ENV, name) ? parse(Int, ENV[name]) : DEFAULT_PORT
 end
 
+"""
+    K8sDeputy.serve!(deputy::Deputy, [host], [port]) -> HTTP.Server
+
+Starts a non-blocking `HTTP.Server` responding to requests to `deputy` health checks. The
+following health check endpoints are available:
+
+- `/health/live`: Is the server is alive/running?
+- `/health/ready`: Is the server is ready (has `readied(deputy)` been called)?
+
+These endpoints will respond with HTTP status `200 OK` on success or
+`503 Service Unavailable` on failure.
+"""
 function serve!(deputy::Deputy, host=localhost, port::Integer=_default_port())
     router = HTTP.Router()
     HTTP.register!(router, "/health/live", liveness_endpoint(deputy))
