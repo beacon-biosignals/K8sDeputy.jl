74e0c488accb463c2c1f2550a3ec25a2735b6146
Curtis Vogt <curtis.vogt@gmail.com>
2024-03-06 12:12:37 -0600

    Add integration testing scaffolding

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,16 +7,41 @@ using Mocking: Mocking, @mock, @patch, apply
 using Sockets: localhost
 using Test
 
+const RUN_TESTS = let
+    valid_types = ("unit", "integration", "quality_assurance")
+    run_tests = get(ENV, "RUN_TESTS", "unit,quality-assurance")
+    types = map(t -> replace(t, '-' => '_'), split(run_tests, ','))
+    NamedTuple([Symbol(t) => t in types for t in valid_types])
+end
+
 # https://en.wikipedia.org/wiki/Ephemeral_port
 const EPHEMERAL_PORT_RANGE = 49152:65535
 
 Mocking.activate()
 
 @testset "K8sDeputy.jl" begin
-    @testset "Aqua" begin
-        Aqua.test_all(K8sDeputy; ambiguities=false)
+    if RUN_TESTS.quality_assurance
+        @testset "Quality Assurance" begin
+            Aqua.test_all(K8sDeputy; ambiguities=false)
+        end
+    else
+        @warn "Skipping quality assurance tests"
     end
 
-    include("graceful_termination.jl")
-    include("health.jl")
+    if RUN_TESTS.unit
+        @testset "Unit" begin
+            include("graceful_termination.jl")
+            include("health.jl")
+        end
+    else
+        @warn "Skipping unit tests"
+    end
+
+    if RUN_TESTS.integration
+        @testset "Integration" begin
+            include("integration.jl")
+        end
+    else
+        @warn "Skipping integration tests"
+    end
 end
