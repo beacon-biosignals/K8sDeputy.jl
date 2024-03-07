2955a4dc77b6c8716d859f5cb1ba92048262718f
Curtis Vogt <curtis.vogt@gmail.com>
2024-03-07 12:29:28 -0600

    Use shared termination grace period

--- test/integration.jl
+++ test/integration.jl
@@ -2,6 +2,8 @@ const K8S_DEPUTY_IMAGE = get(ENV, "K8S_DEPUTY_IMAGE", "k8s-deputy:integration")
 const K8S_DEPUTY_IMAGE_REPO = first(split(K8S_DEPUTY_IMAGE, ':'; limit=2))
 const K8S_DEPUTY_IMAGE_TAG = last(split(K8S_DEPUTY_IMAGE, ':'; limit=2))  # Includes image digest SHA
 
+const TERMINATION_GRACE_PERIOD_SECONDS = 5
+
 # As a convenience we'll automatically build the Docker image when a user uses `Pkg.test()`.
 # If the environmental variable is set we expect the Docker image has been pre-built.
 if !haskey(ENV, "K8S_DEPUTY_IMAGE")
@@ -15,12 +17,11 @@ end
 # Verify Julia's handling of the `TERM` signal in a K8s environment
 @testset "SIGTERM graceful termination" begin
     chart_name = "integration"
-    grace_period = 3
     overrides = Dict("image.repository" => K8S_DEPUTY_IMAGE_REPO,
                      "image.tag" => K8S_DEPUTY_IMAGE_TAG,
                      "command" => ["julia", "entrypoint.jl"],
                      "lifecycle" => nothing,
-                     "terminationGracePeriodSeconds" => grace_period)
+                     "terminationGracePeriodSeconds" => TERMINATION_GRACE_PERIOD_SECONDS)
 
     local pod, delete_duration
     install_chart(chart_name, overrides) do
@@ -37,7 +38,7 @@ end
     # delete_event_timestamp = parse(DateTime, delete_event.lastTimestamp, dateformat"yyyy-mm-dd\THH:MM:SS\Z")
 
     logs = String(take!(pod.logs))
-    @test delete_duration < grace_period
+    @test delete_duration < TERMINATION_GRACE_PERIOD_SECONDS
     @test contains(logs, "[1] signal (15): Terminated\nin expression starting at")
     @test !any(event -> event.reason == "FailedPreStopHook", get_events(pod).items)
 end
@@ -45,13 +46,12 @@ end
 # Verify Julia's handling of the `TERM` signal in a K8s environment
 @testset "Ignore SIGTERM graceful termination" begin
     chart_name = "integration"
-    grace_period = 3
     # Child processes don't automatically get forwarded signals
     overrides = Dict("image.repository" => K8S_DEPUTY_IMAGE_REPO,
                      "image.tag" => K8S_DEPUTY_IMAGE_TAG,
                      "command" => ["/bin/sh", "-c", "julia entrypoint.jl"],
                      "lifecycle" => nothing,
-                     "terminationGracePeriodSeconds" => grace_period)
+                     "terminationGracePeriodSeconds" => TERMINATION_GRACE_PERIOD_SECONDS)
 
     local pod, delete_duration
     install_chart(chart_name, overrides) do
@@ -63,18 +63,17 @@ end
     end
 
     logs = String(take!(pod.logs))
-    @test delete_duration > grace_period
+    @test delete_duration > TERMINATION_GRACE_PERIOD_SECONDS
     @test !contains(logs, "signal (15): Terminated")
     @test !any(event -> event.reason == "FailedPreStopHook", get_events(pod).items)
 end
 
 @testset "Container halts before preStop completes" begin
     chart_name = "integration"
-    grace_period = 3
     overrides = Dict("image.repository" => K8S_DEPUTY_IMAGE_REPO,
                      "image.tag" => K8S_DEPUTY_IMAGE_TAG,
                      "command" => ["julia", "entrypoint.jl"],
-                     "terminationGracePeriodSeconds" => grace_period)
+                     "terminationGracePeriodSeconds" => TERMINATION_GRACE_PERIOD_SECONDS)
 
     local pod, delete_duration
     install_chart(chart_name, overrides) do
@@ -86,18 +85,17 @@ end
     end
 
     logs = String(take!(pod.logs))
-    @test delete_duration < grace_period
+    @test delete_duration < TERMINATION_GRACE_PERIOD_SECONDS
     @test !contains(logs, "signal (15): Terminated")
     @test any(event -> event.reason == "FailedPreStopHook", get_events(pod).items)
 end
 
 @testset "Missing post Julia entrypoint delay" begin
     chart_name = "integration"
-    grace_period = 3
     overrides = Dict("image.repository" => K8S_DEPUTY_IMAGE_REPO,
                      "image.tag" => K8S_DEPUTY_IMAGE_TAG,
                      "command" => ["/bin/sh", "-c", "julia entrypoint.jl"],
-                     "terminationGracePeriodSeconds" => grace_period)
+                     "terminationGracePeriodSeconds" => TERMINATION_GRACE_PERIOD_SECONDS)
 
     local pod, delete_duration
     install_chart(chart_name, overrides) do
@@ -109,18 +107,17 @@ end
     end
 
     logs = String(take!(pod.logs))
-    @test delete_duration < grace_period
+    @test delete_duration < TERMINATION_GRACE_PERIOD_SECONDS
     @test !contains(logs, "signal (15): Terminated")
     @test any(event -> event.reason == "FailedPreStopHook", get_events(pod).items)
 end
 
 @testset "Valid" begin
     chart_name = "integration"
-    grace_period = 3
     overrides = Dict("image.repository" => K8S_DEPUTY_IMAGE_REPO,
                      "image.tag" => K8S_DEPUTY_IMAGE_TAG,
                      "command" => ["/bin/sh", "-c", "julia entrypoint.jl; sleep 1"],
-                     "terminationGracePeriodSeconds" => grace_period)
+                     "terminationGracePeriodSeconds" => TERMINATION_GRACE_PERIOD_SECONDS)
 
     local pod, delete_duration
     install_chart(chart_name, overrides) do
@@ -132,7 +129,7 @@ end
     end
 
     logs = String(take!(pod.logs))
-    @test delete_duration < grace_period
+    @test delete_duration < TERMINATION_GRACE_PERIOD_SECONDS
     @test !contains(logs, "signal (15): Terminated")
     @test !any(event -> event.reason == "FailedPreStopHook", get_events(pod).items)
 end
