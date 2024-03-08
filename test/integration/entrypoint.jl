84a573864f91d32a471c594418f58316498d9949
Curtis Vogt <curtis.vogt@gmail.com>
2024-03-08 10:50:07 -0600

    Update entrypoint.jl

--- test/integration/entrypoint.jl
+++ test/integration/entrypoint.jl
@@ -4,6 +4,6 @@ using K8sDeputy
 
 deputy = Deputy()
 server = K8sDeputy.serve!(deputy, "0.0.0.0")
-graceful_terminator(() -> shutdown(deputy))
-readied(deputy)
+graceful_terminator(() -> shutdown!(deputy))
+readied!(deputy)
 wait(server)
