59081cdd8d892eb075336fa47ed6b053bc0775d9
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2025-05-02 16:02:47 -0400

    WIP

--- supervise.sh
+++ supervise.sh
@@ -1,56 +1,25 @@
 #!/bin/bash
 
-get_pid()
-{
-    PID_FILE="${IPC_DIR}/julia-entrypoint.pid"
-    until [ -f "${PID_FILE}" ]; do
-        sleep 1
-    done
-    read -r SUPERVISED_PID <"${IPC_DIR}/julia-entrypoint.pid"    
-    echo "supervising $SUPERVISED_PID"
-}
+project=$(realpath $0 | xargs dirname)
 
-get_socket()
-{
-    SOCKET_PATH="${IPC_DIR}/graceful-terminator.${SUPERVISED_PID}.socket"
-    until [ -e "$SOCKET_PATH" ]; do
-        sleep 1
-    done
-
-    if [ ! -S "$SOCKET_PATH" ]; then
-        echo "Expected socket at $SOCKET_PATH; got something else. $SUPERVISED_PID is a zombie now" >&2
-        exit 1
-    fi
-    echo "using socket at $SOCKET_PATH"
-}
+echo "script is running from $project"
+echo "my PID is $$"
 
 terminate_supervised()
 {
-    echo "Gently terminating $SUPERVISED_PID"
-    nc -U "$SOCKET_PATH" <<<"terminate"
-    wait
-    STATUS=$?
-    echo "$SUPERVISED_PID exited with $STATUS"
-    exit $STATUS
+    julia --project="$project" -e 'using K8sDeputy; graceful_terminate()'
+    wait $child
+    exit $?
 }
 
-echo "my PID is $$"
-
-# create a separate IPC dir for this invocation because there can be a race with the PID file with 
-IPC_DIR="${DEPUTY_IPC_DIR:-/run}/supervise-$$"
-echo "IPC_DIR=$IPC_DIR"
-mkdir -p "$IPC_DIR"
-
 # start background process
-DEPUTY_IPC_DIR=$IPC_DIR "$@" &
-
-get_pid
-get_socket
+"$@" &
+child=$!
 
 # TODO: check that nc is available before starting
 # trap TERM to write "terminate" to socket with nc
 trap "terminate_supervised" TERM INT
 
-wait
+wait $child
 
 echo "done"
