ff1548eba0eb84f463971fafc4839694df004cba
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2025-05-02 18:36:34 -0400

    note

--- bin/supervise.sh
+++ bin/supervise.sh
@@ -15,10 +15,17 @@ echo "my PID is $$"
 terminate_supervised()
 {
     echo "Superviser is terminating via K8sDeputy.graceful_terminate()..."
+    # XXX: this load path injection only works if the active project _already
+    # has_ K8sDeputy somewhere in its dependencies.  The purpose of this is to
+    # make this all work _without_ requiring that K8sDeputy is explicitly
+    # included in the project (i.e., to support use cases where it comes in as a
+    # dependency of a bigger library that wraps it along with other features.)
     JULIA_LOAD_PATH="$project:$JULIA_LOAD_PATH" julia -e '@show Base.load_path(); using K8sDeputy; graceful_terminate()'
     wait $child
     exit $?
 }
+
+# TODO: set and validate load path before running?
 
 # start background process
 "$@" &
