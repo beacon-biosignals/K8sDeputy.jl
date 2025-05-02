158084c15c5ccd182d1af9ee6148b76080d7e41c
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2025-05-02 18:26:11 -0400

    WIP

--- bin/supervise.sh
+++ bin/supervise.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# XXX: this assumes that this script's realpath is in the root of the K8sDeputy
-# project.  must be updated if we move to etc/ or bin/ or something.
+# XXX: this assumes that this script's realpath is one subdirectory deep in the
+# K8sDeputy project.  must be updated if we move it any deeper
 #
 # Also `realpath` may not be universally available so may want to figure out
 # something more generic to follow symlinks and resolve relative paths etc.
 project=$(realpath $0 | xargs dirname | xargs dirname)
 # TODO: remove this sanity check when we're confident that it's working
-echo "inferred project=$project" >&2
+echo "inferred project=$project"
 
 # TODO: printing this just to be able to know what to `kill`
 echo "my PID is $$"
